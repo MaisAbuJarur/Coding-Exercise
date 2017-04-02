d91eeab2108d4e5b1344611642d00c125f7d72f7
MaisAbuJarur <maisabujarur@gmail.com>
2017-04-02 15:02:23 +0300

    heroku upload

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ var app = express();
 var request = require('request');
 var path = require('path');
 
+//use built in middleware to serve static files
 app.use(express.static(path.join(__dirname,'dist')));
 
 
+//handle requests for data from api
 var url = "https://offersvc.expedia.com/offers/v2/getOffers?scenario=deal-finder&page=foo&uid=foo&productType=Hotel"
 
 app.get('/api/offers',function(req,res) {
@@ -18,8 +20,9 @@ app.get('/api/offers',function(req,res) {
 
 });
 
+//set express to listen for requests
 
-var port = process.env.PORT || 4000;
+var port = process.env.PORT || 5000;
 app.listen(port, function () {
     console.log('...Server now listening on port ' + port);
   });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,31 @@ import { OffersService } from './offers.service'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  offers : any;
-  Math: any;
 
-  constructor(private Get: OffersService) {
-  this.Get.getAllOffers().subscribe(data=>{
-  	console.log(data)
-    this.offers = data.offers.Hotel;
-    
-  });
-  this.Math = Math
-   }
-}
+export class AppComponent {
+	offers : any;
+	Math: any;
+
+
+	constructor(private Get: OffersService) {
+		this.Get.getAllOffers().subscribe(data=>{
+			this.offers = data.offers.Hotel;
+
+			for (var i = 0; i < this.offers.length; i++) {
+
+				this.offers[i].hotelUrls.hotelInfositeUrl=
+				decodeURIComponent(this.offers[i].hotelUrls.hotelInfositeUrl)
+
+				this.offers[i].hotelUrls.hotelSearchResultUrl = 
+				decodeURIComponent(this.offers[i].hotelUrls.hotelSearchResultUrl)
+
+			}
+		});
+		this.Math = Math;
+
+	}
+
+}  
+
 
 
